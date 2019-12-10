a687f12da8ee92a0a63cc9e41b995e27d5f4f6b1
hppRC <hpp.ricecake@gmail.com>
2019-12-10 13:30:52 +0900

    chore

--- report1/report1.py
+++ report1/report1.py
@@ -6,10 +6,9 @@ FONT_SIZE = 4
 def wirte_message(input_path, message, output_path):
     img = cv.imread(input_path)
     height, width = img.shape[:2]
-    print(len(message))
     # define: putText(img, text, org, fontFace, fontScale, color,
     # thickness=None, lineType=None, bottomLeftOrigin=None)
-    cv.putText(img, message, (width//2 - len(message)*FONT_SIZE, height//2),
+    cv.putText(img, message, (width//4 - len(message)*FONT_SIZE, height//2),
                cv.FONT_HERSHEY_PLAIN, FONT_SIZE, (255, 255, 255),
                5, cv.LINE_AA)
     cv.imwrite(output_path, img)
